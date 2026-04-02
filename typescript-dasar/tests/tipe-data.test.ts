f8f3c8fcb410cf1af0593b1298de92898c6b0898
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-02 14:50:07 +0700

    Tipe Data Primitif

---
+++ typescript-dasar/tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Tipe Data", () => {
+    it("should be able to use string", () => {
+        const name: string = "John Doe";
+        const balance: number = 1000;
+        const isVip: boolean = true;
+        
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
