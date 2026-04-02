d7114e8caf76808c35824fa4bec4f38cc37b4d95
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-02 15:41:15 +0700

    Tipe Data Array

---
+++ typescript-dasar/tests/array.test.ts
@@ -0,0 +1,20 @@
+describe("Array", function() {
+    it("should be able to use array", function() {
+        // Cara 1
+        //const names: Array<string> = ["John", "Doe", "Jane"];
+        //console.log(names);
+        
+        // Array Readonly cara 1
+        const names: ReadonlyArray<string> = ["John", "Doe", "Jane"];
+        console.log(names);
+        
+        
+        // Cara 2
+        //const numbers: string[] = ["John", "Doe", "Jane"];
+        //console.log(numbers);
+        
+        // Array Readonly cara 2
+        const numbers: readonly number[] = [1, 2, 3, 4, 5];
+        console.log(numbers);
+    });
+})
