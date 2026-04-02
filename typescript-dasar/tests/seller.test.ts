807f63f426f7b6a81c85157e970d1f4a71e66839
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-02 20:15:46 +0700

    Interface Readonly Properties

--- typescript-dasar/tests/seller.test.ts
+++ typescript-dasar/tests/seller.test.ts
@@ -4,7 +4,9 @@ describe('Seller', () => {
     it('should be able to create a seller', () => {
         const seller: Seller = {
             id: 1,
-            name: "John"
+            name: "John",
+            nib: "123456789",
+            npwp: "123456789"
         }
         
         console.log(seller);
