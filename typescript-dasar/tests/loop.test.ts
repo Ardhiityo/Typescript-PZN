92a107b20153f5d6424040e93d5bd8d46a870d43
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-03 15:14:49 +0700

    While & Do While Loop

--- typescript-dasar/tests/loop.test.ts
+++ typescript-dasar/tests/loop.test.ts
@@ -20,4 +20,38 @@ describe('Loop', () => {
             console.log(names[name]);
         }
     });
+    
+    it('should be able to create while loop', () => {
+        let i = 0;
+        while(i < 10){
+            console.log(i);
+            i++;
+        }
+    });
+
+    it('should be able to create do while loop', () => {
+        let i = 0;
+        do{
+            console.log(i);
+            i++;
+        }while(i < 10);
+    });
+
+    it('should be able to break loop', () => {
+        for(let i = 0; i < 10; i++){
+            if(i === 5){
+                break;
+            }
+            console.log(i);
+        }
+    });
+
+    it('should be able to continue loop', () => {
+        for(let i = 0; i < 10; i++){
+            if(i % 2 === 0){
+                continue;
+            }
+            console.log(i);
+        }
+    });
 });
