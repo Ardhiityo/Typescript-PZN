4c0b09a1ab6f8ec91d90309d8d1517d652e8d0f6
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-03 13:48:35 +0700

    Function Parameter

--- typescript-dasar/tests/function.test.ts
+++ typescript-dasar/tests/function.test.ts
@@ -12,4 +12,29 @@ describe('Function', () => {
 
         sayHello2("John");
     });
+    
+    it('should be able to create function with default value', () => {
+        function sayHello(name:string = "John"):string{
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello John");
+    });
+
+    it('should be able to create function with rest parameter', () => {
+        function sayHello(name:string, ...hobbies:string[]):string{
+            return `Hello ${name}, hobbies: ${hobbies.join(", ")}`;
+        }
+
+        expect(sayHello("John", "Reading", "Swimming", "Coding")).toBe("Hello John, hobbies: Reading, Swimming, Coding");
+    });
+
+    it('should be able to create function with optional parameter', () => {
+        function sayHello(name:string, age?:number):string{
+            return `Hello ${name}, age: ${age}`;
+        }
+
+        expect(sayHello("John", 25)).toBe("Hello John, age: 25");
+        expect(sayHello("John")).toBe("Hello John, age: undefined");
+    });
 });
