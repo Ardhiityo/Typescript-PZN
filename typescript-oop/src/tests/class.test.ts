d806441442a61c827d1abe2262984cedbbfa30af
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-03 19:11:47 +0700

    Constructor

--- typescript-oop/src/tests/class.test.ts
+++ typescript-oop/src/tests/class.test.ts
@@ -9,4 +9,20 @@ describe('Class', () => {
        expect(customer).toBeInstanceOf(Customer);
        expect(order).toBeInstanceOf(Order);
     });
+
+    it('should can create class with constructor', () => {
+       class Customer {
+            constructor() {
+                console.log('Customer created');
+            }
+       }; 
+       new Customer();
+       
+       class Order {
+            constructor() {
+                console.log('Order created');
+            }
+       }; 
+       new Order();
+    });
 })
