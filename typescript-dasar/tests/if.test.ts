7a9a9823541491b535f71e0f9283d21b14846849
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-03 14:41:20 +0700

    If Statement

---
+++ typescript-dasar/tests/if.test.ts
@@ -0,0 +1,9 @@
+describe('If Statement', () => {
+    it('should be able to create if statement', function () {
+        const value:number = 10;
+
+        if(value === 10){
+            console.log("Value is 10");
+        }
+    });
+});
