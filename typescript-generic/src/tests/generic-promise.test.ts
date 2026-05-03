7faee04fa2fd64e12b63ba1ee6f5f43a9976ccd1
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-03 16:19:28 +0700

    Generic Promise

---
+++ typescript-generic/src/tests/generic-promise.test.ts
@@ -0,0 +1,22 @@
+test('Should support generic promise', async () => {
+    async function fetchData(value: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (value === 'Eko') {
+                    resolve(`Hello ${value}`);
+                } else {
+                    reject(`Not found`);
+                }
+            }, 1000)
+        })
+    }
+
+    const data = await fetchData('Eko');
+    expect(data).toBe('Hello Eko');
+
+    try {
+        await fetchData('eko');
+    } catch (error) {
+        expect(error).toBe('Not found');
+    }
+})
