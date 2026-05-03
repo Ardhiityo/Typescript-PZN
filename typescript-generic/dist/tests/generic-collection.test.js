f57494f4888cdff65175f4814e71b1a06adface6
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-03 15:48:55 +0700

    Generic Collection

---
+++ typescript-generic/dist/tests/generic-collection.test.js
@@ -0,0 +1,25 @@
+test('Should support generic collection of array', () => {
+    const data = new Array();
+    data.push(1);
+    data.push(2);
+    data.push(3);
+    //error
+    //data.push('salah');
+    expect(data).toEqual([1, 2, 3]);
+});
+test('Should support generic collection of set', () => {
+    const data = new Set();
+    data.add('Eko');
+    data.add('Kurniawan');
+    expect(data.size).toBe(2);
+    expect(data.has('Eko')).toBe(true);
+    expect(data.has('Kurniawan')).toBe(true);
+});
+test('Should support generic collection of map', () => {
+    const data = new Map();
+    data.set('Eko', 1);
+    data.set('Kurniawan', 2);
+    expect(data.get('Eko')).toBe(1);
+    expect(data.get('Kurniawan')).toBe(2);
+});
+export {};
